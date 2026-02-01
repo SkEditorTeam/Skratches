8bfd9bccd0a1c6f9f6a8f5bf7bbbecc4fc7dd17a
GliczDev <67753196+GliczDev@users.noreply.github.com>
2026-02-01 10:41:13 +0100

    Exclude deprecated Sonatype snapshot URLs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,14 @@ subprojects {
         maven("https://repo.roxymc.net/snapshots")
         maven("https://repo.papermc.io/repository/maven-public/")
     }
+
+    afterEvaluate {
+        // Some older Skript versions still use deprecated Sonatype snapshot URLs,
+        // which breaks the build. It's easier to just remove them like this and minimize the diff.
+        repositories.removeIf {
+            it is MavenArtifactRepository && it.url.toString().contains("oss.sonatype.org")
+        }
+    }
 }
 
 val skriptVersions: List<String> by gradle.extra
